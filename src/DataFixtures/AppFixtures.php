9f0a36736ebc4cb15955fd35186cf726ad8c66e5
delphine-nguyen <delphine.nguyend@gmail.com>
2024-02-23 09:58:35 +0100

    (chore) Add data fixtures for Product

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,45 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
+use App\Entity\User;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker\Factory::create("fr_FR");
+
+        for ($i = 0; $i < 10; $i++) {
+            $product = new Product();
+
+            $product->setName($faker->word());
+            $product->setDescription($faker->sentence(2, true));
+            $product->setUrlImage($faker->imageUrl());
+            $product->setPrice($faker->randomFloat(2, 0, 100));
+
+            $manager->persist($product);
+        }
+
+        // for ($i = 0; $i < 10; $i++) {
+        //     $user = new User();
+
+        //     $lastname = $faker->lastName();
+        //     $firstname = $faker->firstName();
+
+        //     $user->setName($lastname);
+        //     $user->setFirstname($firstname);
+        //     $user->setEmail("$firstname.$lastname@" . $faker->safeEmailDomain());
+        //     $user->setPassword($faker->password(6));
+        //     $user->setRegistrationDate(new DateTimeImmutable($faker->date("Y-m-d")));
+
+        //     $manager->persist($user);
+        // }
+
 
         $manager->flush();
     }
