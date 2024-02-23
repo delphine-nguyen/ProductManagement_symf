aac1b48988c11d9a231c5f86c181926571fd7748
delphine-nguyen <delphine.nguyend@gmail.com>
2024-02-23 09:35:01 +0100

    (feat) Create database and tables

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # ProductManagement_symf
+
+## Entities
+
+- [ ] Product
+  - [ ] Price
+  - [ ] Description
+  - [ ] URL image
+- [ ] User
+  - [ ] Email
+  - [ ] Password
+  - [ ] Name
+  - [ ] Firstname
+  - [ ] Registration date
+  - [ ] Roles
+    - [ ] Admin
+      - [ ] Create a product
+      - [ ] Edit a product
+      - [ ] Delete a product
+    - [ ] User
+      - [ ] See all products
+      - [ ] See the details of a product
---
+++ src/Entity/Product.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $urlImage = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getUrlImage(): ?string
+    {
+        return $this->urlImage;
+    }
+
+    public function setUrlImage(string $urlImage): static
+    {
+        $this->urlImage = $urlImage;
+
+        return $this;
+    }
+}
